fb0cf00fec98dac00ce27c2ed4dadf1b8f5becd6
youngway630 <youngway630@gmail.com>
2019-09-18 17:12:57 +0800

    Initial commit

---
+++ src/main/java/com/wei/Person.java
@@ -0,0 +1,4 @@
+package com.wei;
+
+public class Person {
+}
---
+++ src/main/kotlin/com/wei/Hello.kt
@@ -0,0 +1,16 @@
+package com.wei
+
+fun main(args: Array<String>) {
+    //println("Hello Kotlin")
+    //Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+
+class Human {
+    fun hello(){
+        println("Hello Kotlin")
+    }
+
+}
