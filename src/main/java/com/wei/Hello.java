feae91e50604b741955b9c7a4b97b872c1715ce4
youngway630 <youngway630@gmail.com>
2019-09-18 22:17:06 +0800

    123

--- src/main/java/com/wei/Hello.java
+++ src/main/java/com/wei/Hello.java
@@ -1,4 +1,10 @@
 package com.wei;
 
 public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        new Person().hello();
+        Person p = new Person();
+        p.hello();
+    }
 }
