455a07da9c2c18093fba1160d7c7bb50a1a0b6bb
youngway630 <youngway630@gmail.com>
2019-09-19 13:39:45 +0800

    java & kotlin variable, data type.

--- src/main/java/com/wei/Hello.java
+++ src/main/java/com/wei/Hello.java
@@ -6,5 +6,15 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Wei";
     }
 }
--- src/main/kotlin/com/wei/Hello.kt
+++ src/main/kotlin/com/wei/Hello.kt
@@ -5,6 +5,12 @@ fun main(args: Array<String>) {
     //Human().hello()
     val h = Human()
     h.hello()
+    var age : Int = 19
+    age = 20
+    var weight = 66.5f
+    var name : String
+            name = "Wei"
+
 }
 
 
